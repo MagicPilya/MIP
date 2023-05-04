56c5cca5df9e59f2d769b41b5f674c27a0026eef
Vladimir <magicpilya@yandex.ru>
2023-05-04 14:17:32 +0300

    Add remote

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -22,9 +22,9 @@ const App = () => {
                     <Suspense fallback={<Spinner/>}>
                         <Routes>
                             <Route path="/MIP"       element={<MainPage/>} />
-                            <Route path="/comics" element={<ComicsPage/>}/>
-                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>  
-                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />
+                            <Route path="/MIP/comics" element={<ComicsPage/>}/>
+                            <Route path="/MIP/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>  
+                            <Route path="/MIP/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />
                             <Route path="*"       element={<Page404/>} />
                         </Routes>
                     </Suspense>
--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -6,7 +6,7 @@ const Page404 = () => {
         <div  style={{width: '100%', height: '100%'}}>
             <img className={'not_found'} src={img} alt="404" />
             <Link className={'not_found_back'}style={{display: 'block', textAlign: 'center', fontWeight: 'bold', fontSize: 24, marginTop: 30}}
-            to="/">Back to the main page</Link>
+            to="/MIP">Back to the main page</Link>
         </div>
     )
 }
